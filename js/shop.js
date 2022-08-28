413ea62e88c81e9cc306ac193c02852a1f003a4f
Pluto954 <93003313+Pluto954@users.noreply.github.com>
2022-08-28 23:38:40 +0300

    more table

--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,4 @@
-var happiness = ["cats","dogs","chocolate","cake","coffee"]
+var happiness = ["cat","dog","chocolate","cake","coffee","book"]
 var buy = window.buy_table
 var sell = window.sell_table
 
@@ -13,28 +13,34 @@ function addElement(parent,type,inner){
 
 addElement(buy,"th","name")
 addElement(buy,"th","amount")
-addElement(buy,"th","price")
-addElement(buy,"th","buy")
+addElement(buy,"th","price").setAttribute("colspan","2")
 
 addElement(sell,"th","name")
 addElement(sell,"th","amount")
-addElement(sell,"th","price")
-addElement(sell,"th","sell")
+addElement(sell,"th","price").setAttribute("colspan","2")
+
 
 happiness.forEach(h=>{
 	var row = document.createElement("tr")
-	addElement(row,"td",h)
+	addElement(row,"td",h).setAttribute("class","name")
 	addElement(row,"td",0)
 	addElement(row,"td",0)
 	var buy_amount=addElement(row,"td")
-	addElement(buy_amount,"input")
+	var input=addElement(buy_amount,"input")
+	input.setAttribute("min","1")
+	input.setAttribute("type","number")
+	buy_amount.setAttribute("class","input")
 	buy.appendChild(row)
 	
 	var row2 = document.createElement("tr")
-	addElement(row2,"td",h)
+	addElement(row2,"td",h).setAttribute("class","name")
 	addElement(row2,"td",0)
 	addElement(row2,"td",0)
 	var sell_amount=addElement(row2,"td")
-	addElement(sell_amount,"input")
+	var input2=addElement(sell_amount,"input")
+	sell_amount.setAttribute("class","input")
+	input2.setAttribute("min","1")
+	input2.setAttribute("type","number")
+
 	sell.appendChild(row2)
 })
