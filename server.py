a8568fac4930a01b108e3f128307e8527e93eb28
Xonok <metsakolla017@gmail.com>
2022-11-15 19:24:24 +0200

    Cleaned up button logic in server

--- server.py
+++ server.py
@@ -1,25 +1,4 @@
-#WEB LOGIC
-#Respond to get requests for any file in the css, img or js folders.
-#Get requests to other folders should return 403(permission denied), except html in main folder.
-#Respond to blank request by redirecting to login screen.
-
-#GAME LOGIC(requests)
-#Login and register. Keep track of users on disk and in memory.
-#Every request requires a session ID. Redirect to login if missing or invalid.
-#Requests needed:
-#Register/Login - username+password hashed. Return session key if successful, 401 if not.
-#Move - session key + coordinates
-#Dock - session key + coordinates
-#Trade - session key + coordinates + buy dict + sell dict, these dicts are name: amount. Ignore invalid values.
-#For Dock and Trade, check location. If no buildings, give "no buildings" page.
-
-#GAME LOGIC(async)
-#Every day at 4AM do these things:
-#Increase resources on tiles that are not full, up to limit.
-#Tick buildings. First calculate if tick can be done, then remove resources, then add.
-#If not enough space, throw away cheapest resources first, of those produced this tick.
-
-import http.server,os,ssl,json,hashlib,sys,copy
+import http.server,os,ssl,json,copy
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse,parse_qs
 from server import io,user,player,func,pop,station,items,factory,ship,defs,structure
@@ -130,24 +109,10 @@ class MyHandler(BaseHTTPRequestHandler):
 						tile["structure"] = copy.deepcopy(defs.structures[tile["structure"]])
 						tile["structure"]["image"] = defs.ships[tile["structure"]["ship"]]["img"]
 			buttons = {
-				"gather":"initial",
-				"drop_all":"none",
-				"dock":"none",
-				"smelt":"none",
-				"brew":"none",
-				"build":"none"
+				"gather": "initial",
+				"drop_all": "initial" if len(pitems) else "none",
+				"dock": "initial" if tstructure else "none"
 			}
-			if len(pitems):
-				buttons["drop_all"] = "initial"
-			if tstructure:
-				buttons["dock"] = "initial"
-			if pgear.get("mini_smelter"):
-				buttons["smelt"] = "initial"
-			if pgear.get("mini_brewery"):
-				buttons["brew"] = "initial"
-			if pgear.get("station_kit") and not tile_station and not tile_market:
-				buttons["build"] = "initial"
-			buttons["build"] = "initial"
 			pdata.get_space()
 			msg = {"tiles":tiles,"pdata":pdata,"buttons":buttons,"structure":structinfo}
 			self.send_msg(200,json.dumps(msg))
