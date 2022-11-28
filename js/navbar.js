0d9e26c94afe2877880b112fac1a2ee0f52846a6
Xonok <metsakolla017@gmail.com>
2022-11-28 22:30:17 +0200

    Fixed navbar more

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,7 @@
 var path = top.window.location.pathname.split(".")[0].substring(1)
 window[path].className += " active"
 window[path].onclick = null
-console.log(path)
+window.frameElement.style.height = document.documentElement.offsetHeight+"px"
 function change_page(btn){
 	top.window.location.href = '/'+btn.id+'.html'+window.location.search
 }
