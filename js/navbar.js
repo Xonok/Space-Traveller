f84a68e872156070108ecdb7f19c5b3280675625
Xonok <metsakolla017@gmail.com>
2022-11-28 22:00:29 +0200

    Fixed navbar height, removed dead code

--- js/navbar.js
+++ js/navbar.js
@@ -1,26 +1,3 @@
-/*forClass("tablinks",e=>{
-	e.onclick = open_tab
-})
-forClass("tabcontent",el=>{
-	el.style.display = "none"
-})
-
-function open_tab(e) {
-	var tabName = e.target.innerHTML
-	active = e
-	forClass("tabcontent",el=>{
-		el.style.display = "none"
-	})
-	forClass("tablinks",el=>{
-		el.className = el.className.replace(" active", "")
-	})
-	document.getElementById(tabName).style.display = ""
-	e.currentTarget.className += " active"
-	if(tabName!=="Trade"){
-		window.itemtabs.setAttribute("style","display: none")
-	}
-	else{window.itemtabs.setAttribute("style","display: block")}
-}*/
 var path = top.window.location.pathname.split(".")[0].substring(1)
 window[path].className += " active"
 window[path].onclick = null
