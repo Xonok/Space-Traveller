55658774c7d28a9868a7f486c8e2e67ca8116f97
Xonok <metsakolla017@gmail.com>
2022-12-07 07:08:41 +0200

    Dead code

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import http.server,os,ssl,json,copy
 from http.server import BaseHTTPRequestHandler
 from urllib.parse import urlparse,parse_qs
-from server import io,user,player,func,station,items,factory,ship,defs,structure,map,quest
+from server import io,user,player,func,items,factory,ship,defs,structure,map,quest
 
 class MyHandler(BaseHTTPRequestHandler):
 	def do_POST(self):
