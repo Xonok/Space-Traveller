ef8aa98947d537fc05d825f8b64347e23eb6aa30
Xonok <metsakolla017@gmail.com>
2023-01-07 22:48:56 +0200

    Another attempt to fix navbar height

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,15 @@
 var path = top.window.location.pathname.split(".")[0].substring(1)
 window[path].className += " active"
 window[path].onclick = null
-if(document.readyState !== "loading"){
+function fixHeight(){
+	var height = document.documentElement.offsetHeight
+	if(!height){
+		setTimeout(fixHeight,0.01)
+		return
+	}
 	window.frameElement.style.height = document.documentElement.offsetHeight+"px"
 }
-else{
-	document.addEventListener('DOMContentLoaded',()=>{
-		window.frameElement.style.height = document.documentElement.offsetHeight+"px"
-	})
-}
+fixHeight()
 function change_page(btn){
 	top.window.location.href = '/'+btn.id+'.html'+window.location.search
 }
