93410ff3f959b5c477216c47fb50be41e5d4777f
Xonok <metsakolla017@gmail.com>
2023-01-07 22:14:12 +0200

    Attempting to fix navbar disappearing

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,14 @@
 var path = top.window.location.pathname.split(".")[0].substring(1)
 window[path].className += " active"
 window[path].onclick = null
-window.frameElement.style.height = document.documentElement.offsetHeight+"px"
+if(document.readyState !== "loading"){
+	window.frameElement.style.height = document.documentElement.offsetHeight+"px"
+}
+else{
+	document.addEventListener('DOMContentLoaded',()=>{
+		window.frameElement.style.height = document.documentElement.offsetHeight+"px"
+	})
+}
 function change_page(btn){
 	top.window.location.href = '/'+btn.id+'.html'+window.location.search
 }
