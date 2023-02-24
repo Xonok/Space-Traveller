098c37ea68f865ea2cb0cc5d0e3d42f2c37d7c77
Xonok <metsakolla017@gmail.com>
2023-02-24 05:53:21 +0200

    Added support for weighted loot

--- server/loot.py
+++ server/loot.py
@@ -9,6 +9,16 @@ def drop2(table_name,pos):
 	objtile = objmap.get(pos["x"],pos["y"])
 	if "items" not in objtile:
 		objtile["items"] = {}
+	if "single_drop" not in loot_table:
+		rerolls = linear(loot_table,objtile)
+	else:
+		rerolls = weighted(loot_table,objtile)
+	objmap.set(pos["x"],pos["y"],objtile)
+	objmap.save()
+	for name,amount in rerolls.items():
+		for i in range(amount):
+			drop2(name,pos)
+def linear(loot_table,objtile):
 	rerolls = {}
 	for data in loot_table["rolls"]:
 		if random.randint(1,data["rarity"]) == 1:
@@ -21,11 +31,23 @@ def drop2(table_name,pos):
 				if item not in rerolls:
 					rerolls[item] = 0
 				rerolls[item] += random.randint(data["min"],data["max"])
-	objmap.set(pos["x"],pos["y"],objtile)
-	objmap.save()
-	for name,amount in rerolls.items():
-		for i in range(amount):
-			drop2(name,pos)
+	return rerolls
+def weighted(loot_table,objtile):
+	rerolls = {}
+	weights = []
+	for data in loot_table["rolls"]:
+		weights.append(1/data["rarity"])
+	data = random.choices(loot_table["rolls"],weights)
+	item = data["item"]
+	if "reroll" not in data:
+		if item not in objtile["items"]:
+			objtile["items"][item] = 0
+		objtile["items"][item] += random.randint(data["min"],data["max"])
+	else:
+		if item not in rerolls:
+			rerolls[item] = 0
+		rerolls[item] += random.randint(data["min"],data["max"])
+	return rerolls
 def take(data,pdata):
 	pship = ship.get(data["ship"])
 	if pship["owner"] != pdata["name"]: raise error.User("You don't own the ship: "+pship["name"])
