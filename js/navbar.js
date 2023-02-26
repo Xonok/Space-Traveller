0ac25181a9514fb457f22f615c2c07c25f4fa411
Xonok <metsakolla017@gmail.com>
2023-02-26 14:46:33 +0200

    Navbar stuff

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,14 @@
 var path = top.window.location.pathname.split(".")[0].substring(1)
-window[path].className += " active"
-window[path].onclick = null
+if(window[path]){
+	window[path].className += " active"
+	window[path].onclick = null
+}
+if(path !== "battle"){
+	window.battle.style.display = "none"
+}
+if(path === "battle"){
+	window.nav.onclick = null
+}
 function fixHeight(){
 	var height = document.documentElement.offsetHeight
 	if(!height){
