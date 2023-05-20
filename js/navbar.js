5f7cd02cd159f258fdaede5166aacc947fb6ab6b
Pluto954 <93003313+Pluto954@users.noreply.github.com>
2023-05-20 23:03:58 +0300

    made navbar less noisy

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,4 @@
+var f=func
 var path = top.window.location.pathname.split(".")[0].substring(1)
 if(window[path]){
 	window[path].className += " active"
@@ -29,6 +30,8 @@ if(path === "battle"||path==="characters"){
 	}
 	window.items.onclick = null
 	window.items.style.visibility = "hidden"
+	window.settings.onclick = null
+	window.settings.style.visibility = "hidden"
 }
 function fixHeight(){
 	if(document.documentElement.offsetHeight !== 51){
@@ -36,9 +39,51 @@ function fixHeight(){
 	}
 	window.frameElement.style.height = document.documentElement.offsetHeight+"px"
 }
+var div2
+function more_options(){
+	var div=f.addElement(parent.document.body,"div")
+	div2=div
+	div.style.display="none"
+	div.setAttribute("id","options")
+	div.style.position="absolute"
+	div.style.zIndex="1000"
+	f.addElement(div,"label","Create")
+	var items = f.addElement(div,"button","Items")
+	items.setAttribute("id","items")
+	items.onclick=()=>change_page(items)
+	var editor = f.addElement(div,"button","Editor")
+	editor.setAttribute("id","editor")
+	editor.onclick=()=>change_page(editor)
+	var forum = f.addElement(div,"button","Forum")
+	forum.setAttribute("id","forum")
+	forum.onclick=()=>change_page(forum)
+	var chat = f.addElement(div,"button","Chat")
+	chat.setAttribute("id","chat")
+	chat.onclick=()=>change_page(chat)
+	f.addElement(div,"label","In Development")
+	var quests = f.addElement(div,"button","Quests")
+	quests.setAttribute("id","items")
+	quests.onclick=()=>change_page(quests)
+}
+function update_parent(){
+	fixHeight()
+	more_options()
+}
 var ready = document.readyState !== 'loading'
 var listener = ()=>document.addEventListener('DOMContentLoaded', fixHeight)
-ready ? fixHeight() : listener()
+ready ? update_parent() : listener()
 function change_page(btn){
 	top.window.location.href = '/'+btn.id+'.html'+window.location.search
+}
+var visibility
+function visible(){
+		if(visibility){
+			div2.style.display="none"
+			visibility=undefined
+		}
+		else{
+			div2.style.display="flex"
+			visibility=true
+		}	
+	// }
 }
