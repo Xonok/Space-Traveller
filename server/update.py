eeee5e8f58b0a9a56167cb39652ab1834c9b2f9b
Xonok <metsakolla017@gmail.com>
2023-05-28 19:53:33 +0300

    Added a way to remove items, removed hwr devices

--- server/update.py
+++ server/update.py
@@ -1,11 +1,21 @@
 conversions = {
 	"chem_plant": "refinery"
 }
+removals = [
+	"homeworld_return_device",
+	"homeworld_return_device2",
+	"bp_homeworld_return_device",
+	"bp_chem_plant"
+]
 def item_names(table,name):
 	for item,amount in list(table.items()):
 		if item in conversions:
-			print("Updating "+item+" to "+conversions[item]+" in "+name)
+			print("Updating "+item+" to "+item+" in "+name)
 			table[conversions[item]] = amount
+			del table[item]
+	for item,amount in list(table.items()):
+		if item in removals:
+			print("Removing "+item+" from "+name)
 			del table[item]
 def inventory_items(parent):
 	item_names(parent["inventory"]["items"],parent["name"])
