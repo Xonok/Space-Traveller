111002501861cd662eabe9ce34f0f9a5259897c9
Xonok <metsakolla017@gmail.com>
2023-09-21 17:34:46 +0300

    Validation: warning when industry needs item, but planet doesn't trade it

--- server/validation.py
+++ server/validation.py
@@ -7,6 +7,7 @@ def validate():
 	predefs()
 	objects() #wormholes
 	weapons()
+	prices()
 def positions():
 	pships = defs.ships.values()
 	objmaps = defs.objmaps
@@ -160,3 +161,22 @@ def weapons():
 		if data["type"] == "drone":
 			if "ship_predef" not in data:
 				raise Exception("The drone(weapon) "+name+" needs to have a ship_predef.")
+def prices():
+	for name,data in defs.structures.items():
+		if data["type"] != "planet": continue
+		industry_items = []
+		traded_items = []
+		for ind in data["industries"]:
+			ind_def = defs.industries2[ind["name"]]
+			for item in ind_def["input"].keys():
+				if item not in industry_items:
+					industry_items.append(item)
+			for item in ind_def["output"].keys():
+				if item not in industry_items:
+					industry_items.append(item)
+		for item in data.get_prices().keys():
+			if item not in traded_items:
+				traded_items.append(item)
+		for item in industry_items:
+			if item not in traded_items:
+				print("Structure",data["name"],"has industry related to item",item,"but doesn't trade it.")
