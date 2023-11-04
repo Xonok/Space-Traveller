f0eafbe2f7fc81e07caa34ae6717a9736e3208b9
Xonok <metsakolla017@gmail.com>
2023-11-05 00:05:44 +0200

    Images for starters

--- server/Init/ships.py
+++ server/Init/ships.py
@@ -3,6 +3,7 @@ import os
 
 def init():
 	delete_ship_files()
+	starters()
 def delete_ship_files():
 	files = os.listdir(os.path.join("server","data","ships"))
 	for f in files:
@@ -11,3 +12,9 @@ def delete_ship_files():
 			path = os.path.join("server","data","ships",f+".json")
 			print("Deleting unused ship file:",path)
 			os.remove(path)
+def starters():
+	for name,data in defs.starters.items():
+		for data2 in data["ships"]:
+			for name in list(data2.keys()):
+				ship_type = defs.ship_types[name]
+				data2["img"] = ship_type["img"]
