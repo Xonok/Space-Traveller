9684c88d2f7596b3b67499e407f09876452e076e
Xonok <metsakolla017@gmail.com>
2023-12-29 09:42:00 +0200

    Anomaly märkmed

---
+++ server/anomaly.py
@@ -0,0 +1,16 @@
+
+#anomalies are objects in random places on the map
+#when investigated, an event happens. Events can mean several things:
+#*a choice
+#*loot
+#*enemies spawn and attack
+#*player chooses to cancel
+#each is accompanied by text
+#choices essentially redirect to other events
+#when the event chain is exhausted, the anomaly disappears.
+#rewards should only happen at the end, to prevent exploits.
+
+#anomalies appear randomly on any tile. 
+#there can be restrictions on what tiles are valid
+#anomalies are visible on the map, with multiple options for images
+#there should be a limit on the total amount of anomalies, and on those of any given type
