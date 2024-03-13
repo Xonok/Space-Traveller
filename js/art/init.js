23bdd3bedbb45ac0698189782b08825549c2b7b1
Pluto954 <93003313+Pluto954@users.noreply.github.com>
2024-03-13 17:56:35 +0200

    art page prettier

--- js/art/init.js
+++ js/art/init.js
@@ -5,22 +5,27 @@ if(!key){
 	throw new Error("Not logged in.")
 }
 
-var list_box = f.addElement(document.body,"div")
-list_box.classList.add("vertical")
+var list_box = window.buttons
 Object.entries(art.entries).sort((a,b)=>-(a[1].priority>b[1].priority)).forEach(e=>{
 	var name = e[0]
 	var data = e[1]
-	var btn_box = f.addElement(list_box,"div")
-	var btn = f.addElement(btn_box,"button",data.name)
+	var btn = f.addElement(list_box,"button",data.name)
+	btn.style.width="100px"
+	btn.style.margin="10px"
 	var box = f.addElement(list_box,"div")
 	box.classList.add("vertical")
+	box.style.border="2px solid white"
+	box.style.margin="10px"
 	box.style.display = "none"
 	btn.onclick = ()=>{box.style.display=box.style.display==="initial" ? "none" : "initial"}
 	f.addElement(box,"div",data.desc)
 	f.addElement(box,"div",data.concept)
 	Object.entries(data.concept_art||{}).forEach(a=>{
 		var box2 = f.addElement(box,"div")
-		box.classList.add("horizontal")
+		box2.style.border="1px yellow dotted"
+		box2.style.margin="5px"
+		box2.style.marginLeft="80px"
+		box2.classList.add("horizontal")
 		var img = f.addElement(box2,"img")
 		img.src = a[0]
 		img.style.maxWidth = "200px"
