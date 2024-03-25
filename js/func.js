3de8ca54cfc8bb55f4e7197d2f3eeb43848e68df
Xonok <metsakolla017@gmail.com>
2024-03-25 21:14:21 +0200

    Fix open elements showing closed button

--- js/func.js
+++ js/func.js
@@ -348,9 +348,8 @@ if(typeof func === "undefined"){
 				var [name_closed,name_open] = e.innerHTML.split("//")
 				var default_state = e.getAttribute("default") || "closed"
 				var toggle_status = default_state === "open"
-				e.innerHTML = name_closed
+				e.innerHTML = toggle_status ? name_open : name_closed
 				func.forClass(e.getAttribute("toggle"),e=>{
-					e.style.display = "none"
 					e.style.display = default_state === "open" ? "initial" : "none"
 				})
 				e.onclick = ()=>{
