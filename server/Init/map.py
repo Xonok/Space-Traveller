0f9b429a3be61a039a3c1d28a08a73bde39388fc
Xonok <metsakolla017@gmail.com>
2024-05-26 18:38:30 +0300

    Fix

--- server/Init/map.py
+++ server/Init/map.py
@@ -1,10 +1,13 @@
-from server import defs
+from server import defs,gathering
 
 def init():
+	to_update = []
 	for sys_name,sys_data in defs.objmaps.items():
 		for x,col in sys_data["tiles"].items():
 			for y,tile in col.items():
 				basetiles = defs.systems[sys_name]["tiles"]
 				if x not in basetiles or y not in basetiles[x]:
-					gathering.update_resources(sys_name,x,y)
+					to_update.append((sys_name,x,y))
 					print("Illegal tile:",sys_name,x,y,tile)
+	for sys_name,x,y in to_update:
+		gathering.update_resources(sys_name,x,y)
