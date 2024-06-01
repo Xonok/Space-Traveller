fbaacc78eb49807b0f36b86acdfed934420d5e85
Xonok <metsakolla017@gmail.com>
2024-06-01 23:04:12 +0300

    Show more info in station kit description

--- server/Update/item.py
+++ server/Update/item.py
@@ -16,6 +16,11 @@ def station_kits():
 		if data["type"] != "station_kit": continue
 		ship_type = defs.station_kits[item]["ship"]
 		shipdef = defs.ship_types[ship_type]
+		data["props"]["room"] = shipdef["room"]
+		data["props"]["hull"] = shipdef["hull"]
+		if "tracking" in shipdef:
+			data["props"]["tracking"] = shipdef["tracking"]
+		data["desc"] = shipdef["desc"]
 		if len(shipdef["slots"]):
 			data["props"]["slots"] = copy.deepcopy(shipdef["slots"])
 		else:
