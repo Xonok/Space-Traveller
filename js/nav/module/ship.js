fc8989031c038f360576752b1744e98fb6c4c0d8
Xonok <metsakolla017@gmail.com>
2024-06-26 22:55:40 +0300

    Show XP percentage in nav

--- js/nav/module/ship.js
+++ js/nav/module/ship.js
@@ -2,7 +2,8 @@ nav.ship = {
 	update_vitals(){
 		var tab = "&nbsp;&nbsp;&nbsp;&nbsp;"
 		var {hull,armor,shield} = pship.stats
-		window.vitals.innerHTML = "Level: "+cdata.level+tab
+		var xp_percent = Math.round(cdata.xp/10)
+		window.vitals.innerHTML = "Level: "+cdata.level+" ("+xp_percent+"%)"+tab
 		window.vitals.innerHTML += "Hull: "+hull.current+"/"+hull.max+tab
 		window.vitals.innerHTML += "Armor: "+armor.current+"/"+armor.max+tab
 		window.vitals.innerHTML += "Shield: "+shield.current+"/"+shield.max
