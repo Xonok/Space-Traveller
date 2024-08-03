41bb44ca3fa472967696f18bd59a64b92e5ed268
Xonok <metsakolla017@gmail.com>
2024-08-03 14:59:21 +0300

    Cleanup

--- server/loot.py
+++ server/loot.py
@@ -110,7 +110,6 @@ def take(data,cdata):
 	omap = map.otiles(pos["system"])
 	otile = omap.get(pos["x"],pos["y"])
 	if "items" not in otile: raise error.User("This tile doesn't have any items.")
-	print(titems)
 	for item,amount in titems.items():
 		available = otile["items"].get(item,-1)
 		if available == -1: continue
