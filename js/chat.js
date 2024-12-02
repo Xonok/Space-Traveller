dd9e9814b4024ed5906e2fcb1a6adb49f2c2a2db
Xonok <metsakolla017@gmail.com>
2024-12-02 21:14:04 +0200

    Really basic and bad version of chat

--- js/chat.js
+++ js/chat.js
@@ -10,11 +10,50 @@ if(!key){
 	window.location.href = "/login.html"
 	throw new Error("Not logged in.")
 }
-var sock = new WebSocket("wss://"+location.host+"/chat_async")
-sock.onopen = e=>{
-	console.log(e)
-	sock.send(JSON.stringify({"msg":"hello"}))
+
+var chat_socket
+var last_message_idx = -1
+
+function chat_connect(){
+	chat_socket = new WebSocket("wss://"+location.host+"/chat_async")
+	
+	chat_socket.onopen = e=>{
+		console.log(e)
+		// chat_socket.send(JSON.stringify({"msg":"hello"}))
+		chat_command("get-messages",{"idx":last_message_idx})
+	}
+	chat_socket.onmessage = e=>{
+		var msg = JSON.parse(e.data)
+		var evt = msg.event
+		var data = msg.data
+		console.log(e,evt,data)
+		if(!evt){
+			console.log(e)
+			return
+		}
+		if(evt === "msg-receive"){
+			window.chat_log.innerHTML += msg.data.sender+msg.data.time+msg.data.txt+"<br>"
+		}
+		if(evt === "msg-receive-multi"){
+			msg.data.forEach(d=>{
+				window.chat_log.innerHTML += d.sender+d.time+d.txt+"<br>"
+			})
+		}
+	}
+	chat_socket.onerror = e=>{
+		chat_socket.close()
+	}
+	chat_socket.onclose = e=>{
+		setTimeout(chat_connect,1000)
+	}
 }
-sock.onmessage = e=>{
-	console.log(e)
+function chat_command(command,data){
+	data.command = command
+	chat_socket.send(JSON.stringify(data))
+}
+chat_connect()
+window.btn_chat_send.onclick = e=>{
+	var msg = window.input_chat_msg.value
+	if(!msg){return}
+	chat_command("send-message",{"txt":msg})
 }
--- server/chat.py
+++ server/chat.py
@@ -2,19 +2,62 @@ import time,json
 import websocket #locally available
 from server import error
 
-
-def validate_msg(msg):
-	pass
-def do_GET(self):
-	ws.add_client(self)
+commands = {}
+messages = []
+clients = []
+#TODO: validate command params before passing them to handler
+def register_command(name,handler):
+	if name in commands:
+		raise Exception("Trying to register same command twice.")
+	commands[name] = handler
+def get_messages(self,client,idx=int):
+	data = {
+		"event": "msg-receive-multi",
+		"data": messages
+	}
+	ws.send_msg(client,data)
+def send_message(self,client,txt=str):
+	now = time.time()
+	idx = len(messages)+1
+	msg_data = {
+		"txt": txt,
+		"time": time.time(),
+		"idx": idx,
+		"sender": "???"
+	}
+	messages.append(msg_data)
+	data = {
+		"event": "msg-receive",
+		"data": msg_data
+	}
+	for c in clients:
+		ws.send_msg(client,data)
+register_command("get-messages",get_messages)
+register_command("send-message",send_message)
+def do_GET(client):
+	clients.append(client)
+	ws.add_client(client)
 def recv_handler(self,client,msg):
 	try:
 		data = json.loads(msg)
 		if "command" not in data:
 			raise error.User("No command in message."+msg)
-		# if data["command"] not in commands:
-			# raise error.User("Unknown command: "+data["command"])
-		# commands[data["command"]](self,client,data)
+		if data["command"] not in commands:
+			raise error.User("Unknown command: "+data["command"])
+		cmd_name = data["command"]
+		cmd_data = commands[cmd_name]
+		del data["command"]
+		# for key in data:
+			# if key not in cmd_data:
+				# raise error.User("Unnecessary parameter "+key+" for command "+cmd_name)
+		# for key,val in cmd_data.items():
+			# if type(data[key]) != val:
+				# if type(val) == str:
+					# param_type = val
+				# else:
+					# param_type = val.__name__
+				# raise error.User("Parameter "+key+" for command "+cmd_name+" must be of type "+param_type)
+		commands[cmd_name](self,client,**data)
 	except error.User as e:
 		ws.send_error(client,str(e))
 	except Exception as e:
@@ -25,5 +68,6 @@ def drop_handler(self,client):
 	# if client.uname not in world_players[client.world]: return
 	# if client not in world_players[client.world][client.uname]: return
 	# world_players[client.world][client.uname].remove(client)
+	clients.remove(client)
 	print("dropped client",client)
 ws = websocket.Handler(recv_handler,drop_handler)
