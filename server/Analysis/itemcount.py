e1e95f09fabced2e12acdb1472cb4c6700d169be
Xonok <metsakolla017@gmail.com>
2025-02-15 11:16:29 +0200

    Also filter for missing images

--- server/Analysis/itemcount.py
+++ server/Analysis/itemcount.py
@@ -21,8 +21,23 @@ def run():
 		process(data.get_gear())
 		counts[data["ship"]] += 1
 	counts = dict(sorted(counts.items(),key=lambda e: e[1],reverse=True))
+	counts_noimg = {}
+	for item,amount in counts.items():
+		if item in defs.items:
+			idata = defs.items[item]
+		elif item in defs.ship_types:
+			idata = defs.ship_types[item]
+		else:
+			raise Exception("Unknown item or ship: "+item)
+		img_path = idata["img"]
+		if not os.path.exists(img_path):
+			counts_noimg[item] = amount
 	path = os.path.join("output","item_counts.json")
+	path2 = os.path.join("output","item_counts_noimg.json")
 	io.check_dir(path)
 	with open(path,"w") as f:
 		json.dump(counts,f,indent="\t")
+	with open(path2,"w") as f:
+		json.dump(counts_noimg,f,indent="\t")
+	
 run()
