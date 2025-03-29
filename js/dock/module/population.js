2da2f2cefe1370c005455c8d5ad677eb260b3628
Xonok <metsakolla017@gmail.com>
2025-03-29 15:54:44 +0200

    Even better

--- js/dock/module/population.js
+++ js/dock/module/population.js
@@ -25,17 +25,11 @@ function update_pop(){
 		var def = industry_defs[i.name]
 		var el = window.industries
 		var tab = "&nbsp;&nbsp;&nbsp;&nbsp;"
-		var rules = {
-			"primary": "Type: Primary",
-			"secondary": "Type: Secondary",
-			"tertiary": "Type: Tertiary",
-			"special": "Type: Special",
-			"default": "Type: Unknown"
-		}
 		pop += i.workers
 		console.log(i)
 		var change = i.growth + i.migration
 		var ind_div = f.addElement(el,"div",def.name_display+": ")
+		ind_div.style.marginBottom = "5px"
 		var ind_div_workers = f.addElement(ind_div,"span",i.workers)
 		f.addElement(ind_div,"span"," workers")
 		if(change > 0){
@@ -56,36 +50,52 @@ function update_pop(){
 		tt_txt += "<div>Min: "+def.min+"</div>"
 		tt_txt += "Growth: <label "+style_growth+">"+i.growth+"</label>"
 		tt_txt += "Migration: <label "+style_migration+">"+i.growth+"</label>"
-		tt_txt += rules[def.type] || rules[def["default"]]
 		f.tooltip2(ind_div_workers,tt_txt)
-		if(Object.keys(def.input).length){
-			var box = f.addElement(el,"div")
-			box.classList.add("horizontal")
-			box.innerHTML = tab+"Inputs: "
-			var keys = Object.keys(def.input)
-			keys.forEach((k,idx)=>{
-				var img_box = f.img_box(box,"17px","17px",idata[k].img)
-				img_box.style.marginRight = "2px"
-				box.innerHTML += idata[k].name
-				if(idx < keys.length-1){
-					box.innerHTML += ",&nbsp"
-				}
-			})
-			var box2 = f.addElement(el,"div")
-			box2.classList.add("horizontal")
-			if(Object.keys(def.output).length){
-				box2.innerHTML = tab+"Outputs: "
-			}
+		var box = f.addElement(ind_div,"div",tab)
+		box.classList.add("horizontal")
+		var keys = Object.keys(def.input)
+		var img_stack = f.addElement(box,"div")
+		img_stack.classList.add("vertical")
+		img_stack.style.minWidth = "7rem"
+		keys.forEach((k,idx)=>{
+			var img_frame = f.addElement(img_stack,"div")
+			img_frame.classList.add("horizontal")
+			var img_box = f.img_box(img_frame,"17px","17px",idata[k].img)
+			img_box.style.marginRight = "2px"
+			img_frame.innerHTML += idata[k].name
+		})
+		var arrow_div = f.addElement(box,"div","&nbsp&#10596;&nbsp")
+		arrow_div.style.fontSize = "1.5em"
+		arrow_div.style.marginTop = "auto"
+		arrow_div.style.marginBottom = "auto"
+		var img_stack2 = f.addElement(box,"div")
+		if(Object.keys(def.output).length){
 			var keys2 = Object.keys(def.output)
 			keys2.forEach((k,idx)=>{
-				var img_box = f.img_box(box2,"17px","17px",idata[k].img)
+				var img_frame = f.addElement(img_stack2,"div")
+				img_frame.classList.add("horizontal")
+				var img_box = f.img_box(img_frame,"17px","17px",idata[k].img)
 				img_box.style.marginRight = "2px"
-				box2.innerHTML += idata[k].name
-				if(idx < keys2.length-1){
-					box2.innerHTML += ",&nbsp"
-				}
+				img_frame.innerHTML += idata[k].name
 			})
 		}
+		else{
+			var img_frame = f.addElement(img_stack2,"div")
+			img_frame.classList.add("horizontal")
+			var img_box = f.img_box(img_frame,"17px","17px","img/credits.webp")
+			img_box.style.marginRight = "2px"
+			img_frame.innerHTML += "Credits"
+		}
+		ind_div.innerHTML += tab
+		var rules = {
+			"primary": "Type: Primary",
+			"secondary": "Type: Secondary",
+			"tertiary": "Type: Tertiary",
+			"special": "Type: Special",
+			"default": "Type: Unknown"
+		}
+		ind_div.innerHTML += rules[def.type] || rules[def["default"]]
+		ind_div.innerHTML += "<br>"
 	})
 	window.total_pop.innerHTML = pop ? "<br>Total population: <b>"+pop : "</b>" 
 }
