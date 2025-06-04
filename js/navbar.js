0544baab7ce430927b3aa913d3e07ee142b5dddb
Pluto954 <93003313+Pluto954@users.noreply.github.com>
2025-06-04 22:09:40 +0300

    navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,54 +1,65 @@
 var prev_view
 function navbar_update(view_id){
-	var pluto = false
-	if(pluto){
-		var path = view_id
-		if(window["bar_"+path]){
-			window["bar_"+path].className += " active"
-			// window["bar_"+path].onclick = null
-		}
-		// if(path === "nav"){
-			// window.bar_dock.onclick = e=>{
-				// if(!tile?.structure){
-					// e.preventDefault()
-					// window.error_display.innerHTML = "Can't dock. There is no structure here."
-				// }
-			// }
-		// }
-		if(path !== "battle"){
-			window.bar_battle.style.display = "none"
-		}
-		if(path !== "dock"){
-			window.bar_dock.style.display = "none"
-		}
-
-		if(path === "battle"||path==="characters"){
-			// window.bar_nav.onclick = null
-			window.bar_nav.style.visibility = "hidden"
-			// window.bar_dock.onclick=null
-			window.bar_dock.style.visibility = "hidden"
-			// window.bar_more.onclick = null
-			window.bar_more.style.visibility = "hidden"
-			if(path==="battle"){
-				// window.bar_characters.onclick = null
-				window.bar_characters.style.visibility = "hidden"
-			}
-			if(path==="characters"){
-				// window.bar_battle.onclick = null
-				window.bar_battle.style.visibility = "hidden"
-			}
-			// window.bar_settings.onclick = null
-			window.bar_settings.style.visibility = "hidden"
-		}
-		var prev_button
-		if(path !=="nav" && path !=="settings" && path !=="character" && window["bar_"+path]){
-			window["bar_"+path].style.display="initial"
-			prev_button=path
-		}
-		else if(prev_button && prev_button!=="nav" && prev_button !=="settings" && prev_button !=="character" ){
-			window["bar_"+prev_button].style.display="none"
-		}
+	// var pluto = false
+	// if(pluto){
+	var path = view_id
+	console.log(view_id,q.in_battle)
+	if(path==="characters"){
+		// window.bar_battle.onclick = null
+		window.bar_battle.style.visibility = "hidden"
+		window.bar_profile.style.visibility = "hidden"
+		window.bar_wiki.style.visibility = "hidden"
+		window.bar_quests.style.visibility = "hidden"
+		window.bar_nav.style.visibility = "hidden"
+		window.bar_map.style.visibility = "hidden"
+		window.bar_dock.style.visibility = "hidden"
 	}
+	else if(q.in_battle){
+		window.bar_battle.style.visibility = "visible"
+		window.bar_profile.style.visibility = "hidden"
+		window.bar_wiki.style.visibility = "hidden"
+		window.bar_quests.style.visibility = "hidden"
+		window.bar_nav.style.visibility = "hidden"
+		window.bar_map.style.visibility = "hidden"
+		window.bar_dock.style.visibility = "hidden"
+	}
+	else{
+		window.bar_battle.style.display = "none"
+		window.bar_profile.style.visibility = "visible"
+		window.bar_wiki.style.visibility = "visible"
+		window.bar_quests.style.visibility = "visible"
+		window.bar_nav.style.visibility = "visible"
+		window.bar_map.style.visibility = "visible"
+		window.bar_dock.style.visibility = "visible"
+	}
+	if(window["bar_"+path]){
+		window["bar_"+path].className += " active"
+	}
+	if(path !== "dock"){
+		window.bar_dock.style.display = "none"
+	}
+	// if(path === "battle"){
+		// window.bar_nav.onclick = null
+		// window.bar_nav.style.visibility = "hidden"
+		// window.bar_dock.onclick=null
+		// window.bar_dock.style.visibility = "hidden"
+		// window.bar_more.onclick = null
+		// window.bar_more.style.visibility = "hidden"
+		// window.bar_characters.onclick = null
+		// window.bar_characters.style.visibility = "hidden"
+		// window.bar_settings.onclick = null
+		// window.bar_settings.style.visibility = "hidden"
+	// }
+	
+	// var prev_button
+	// if(path !=="nav" && path !=="settings" && path !=="character" && window["bar_"+path]){
+	// 	window["bar_"+path].style.display="initial"
+	// 	prev_button=path
+	// }
+	// else if(prev_button && prev_button!=="nav" && prev_button !=="settings" && prev_button !=="character" ){
+	// 	window["bar_"+prev_button].style.display="none"
+	// }
+	// }
 	var id = "bar_"+view_id
 	if(prev_view){
 		prev_view.classList.remove("active")
