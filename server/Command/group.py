3910b6222ee907661110f0c3b31ef5b421391b31
Xonok <metsakolla017@gmail.com>
2025-06-29 08:04:01 +0300

    Cleanup

--- server/Command/group.py
+++ server/Command/group.py
@@ -3,42 +3,37 @@ from server import Group,error
 
 def do_get_group_data(cdata):
 	pass
-def do_group_create(cdata,name="str"):
-	return Group.create(name,cdata["name"])
-def do_group_apply(cdata,group="str",reason="str"):
-	return Group.apply(group,cdata["name"],reason)
-def do_group_accept(cdata,name="str"):
-	cname = cdata["name"]
+def do_group_create(cname,name="str"):
+	return Group.create(name,cname)
+def do_group_apply(cname,group="str",reason="str"):
+	return Group.apply(group,cname,reason)
+def do_group_accept(cname,name="str"):
 	group = Group.of_char(cname)
 	if not group:
 		raise error.User("You are not in any group.")
 	if group["leader"] != cname:
 		raise error.User("Only the leader can accept applications.")
 	return Group.accept(group["id"],name)
-def do_group_deny(cdata,name="str"):
-	cname = cdata["name"]
+def do_group_deny(cname,name="str"):
 	group = Group.of_char(cname)
 	if not group:
 		raise error.User("You are not in any group.")
 	if group["leader"] != cname:
 		raise error.User("Only the leader can deny applications.")
 	return Group.deny(group["id"],name)
-def do_group_leave(cdata):
-	cname = cdata["name"]
+def do_group_leave(cname):
 	group = Group.of_char(cname)
 	if not group:
 		raise error.User("You are not in any group.")
 	return Group.leave(group["id"],cname)
-def do_group_kick(cdata,name="str"):
-	cname = cdata["name"]
+def do_group_kick(cname,name="str"):
 	group = Group.of_char(cname)
 	if not group:
 		raise error.User("You are not in any group.")
 	if group["leader"] != cname:
 		raise error.User("Only the leader can kick people.")
 	return Group.kick(group["id"],name)
-def do_group_transfer(cdata,new_leader="str"):
-	cname = cdata["name"]
+def do_group_transfer(cname,new_leader="str"):
 	group = Group.of_char(cname)
 	if not group:
 		raise error.User("You are not in any group.")
@@ -67,10 +62,3 @@ api.register("group-kick",do_group_kick,"group")
 api.register("group-transfer",do_group_transfer,"group")
 api.register("group-modify-ranks",do_modify_ranks,"group")
 api.register("group-assign-rank",do_assign_rank,"group")
-
-# def create(name,leader):
-# def apply(g_id,name,reason):
-# def accept(g_id,name):
-# def deny():
-# def leave(g_id,name):
-# def transfer(g_id,new_leader):
