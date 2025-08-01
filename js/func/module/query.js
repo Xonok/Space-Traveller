360586dabbead9d7c6e62f8acbe4a9a018bffbab
Xonok <metsakolla017@gmail.com>
2025-08-01 23:50:05 +0300

    Notes for smarter idata caching

--- js/func/module/query.js
+++ js/func/module/query.js
@@ -4,6 +4,13 @@ query = {
 	},
 	events: {},
 	receive(msg){
+		//Make a function for each query that exists.
+		//Have the server send a list of query names,
+		//error here if the function doesn't exist.
+		//Each function should determine what idata can be extracted from the query response.
+		//Once all parts of the message have been analyzed, send a single request for idata.
+		//While idata isn't available, don't run any view-specific code.
+		//It would be nice if there was a distinction between query responses and command responses.
 		Object.entries(msg).forEach(e=>query.data[e[0].replace("-","_")] = e[1])
 		Object.keys(msg).forEach(k=>{
 			if(query.events[k]){
