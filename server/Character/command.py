d0d3c7a509f3164d0a07c0ab4f8b425f36c3e767
Xonok <metsakolla017@gmail.com>
2025-08-10 09:43:56 +0300

    Boost the command provided by levels 4 and 5

--- server/Character/command.py
+++ server/Character/command.py
@@ -3,7 +3,7 @@ from server import ship,defs,stats
 slots_by_swarm_level = [0,1,3,6,10,15,21,28,36,45,55,66,78,91,105,120,136,153,171,190,210]
 ships_by_command_level = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
 
-slots_by_command = [0,2,4,6,8,12]
+slots_by_command = [0,2,4,6,10,14]
 slot_req = [2,4,6,8,12,16]
 slot_req_swarm = [2,4,6,8,12,16]
 def update_command_slots(cdata):
